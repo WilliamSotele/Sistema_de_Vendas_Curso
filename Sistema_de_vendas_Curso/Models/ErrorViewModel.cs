964f77eb16e8c390db0ee8123585944d4b6a86e6
William Sotele <willsotele@gmail.com>
2019-08-20 15:26:45 -0300

    Adicionar arquivos de projeto.

---
+++ Sistema_de_vendas_Curso/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sistema_de_vendas_Curso.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
