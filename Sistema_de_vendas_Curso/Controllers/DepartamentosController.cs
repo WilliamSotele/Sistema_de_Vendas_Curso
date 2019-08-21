f535c13dc98cf80ba598a1468ba52b7ed8ec2b68
William Sotele <willsotele@gmail.com>
2019-08-21 13:50:43 -0300

    Criado alguns departamentos

---
+++ Sistema_de_vendas_Curso/Controllers/DepartamentosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sistema_de_vendas_Curso.Models;
+
+namespace Sistema_de_vendas_Curso.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> lista = new List<Departamento>();
+            lista.Add(new Departamento (1,"Eletrônicos"));
+            lista.Add(new Departamento(2, "Ferramentas"));
+            lista.Add(new Departamento(3, "Plasticificados"));
+
+            return View(lista);
+        }
+    }
+}
---
+++ Sistema_de_vendas_Curso/Models/Departamento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema_de_vendas_Curso.Models
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public Departamento(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+    }
+}
